8f2ff2e47c476563f265b912e216642f940a2dde
Ruka Okuyama <rukaokuyama@gmail.com>
2023-03-22 17:46:00 +0900

    added function to get items

---
+++ main.js
@@ -0,0 +1,15 @@
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+let items = getItems();
+
+function getItems() {
+  const value = localStorage.getItem("todo-test") || "[]";
+
+  return JSON.parse(value);
+}
+
+
+
+console.log(items);
